c0989f681995bc245f752543e10c52c36bcedd6e
David Siaw <davidsiaw@gmail.com>
2018-07-25 17:37:08 +0900

    initial commit

---
+++ deploy.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+git config --global user.email "davidsiaw@gmail.com"
+#git config --global user.name "David Siaw (via Circle CI)"
+
+git clone git@github.com:davidsiaw/davidsiaw.net.git build
+cp -r build/.git ./gittemp
+bundle exec weaver build -r https://davidsiaw.net
+cp -r ./gittemp build/.git
+pushd build
+echo davidsiaw.net > CNAME
+cp 404/index.html 404.html
+git add .
+git add -u
+git commit -m "update `date`"
+ssh-agent bash -c 'ssh-add ~/.ssh/id_github.com; git push'
+popd
+
